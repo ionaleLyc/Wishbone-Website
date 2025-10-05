42e7b00fe7f3bfa1cefe74b95d408670debb1da1
ionaleLyc <southiyd143@gmail.com>
2025-10-06 00:06:45 +0300

    Верстка второй секции под размеры макета (без адаптива)

---
+++ src/components/OurFirm/OurFirm.jsx
@@ -0,0 +1,44 @@
+import './OurFirm.css'
+import stephen_collier from '../../assets/people/stephen_collier.png'
+
+function OurFirm() {
+    return (
+        <div className='our-firm-section'>
+
+            <div className='our-firm-wrapper'>
+
+                <div className='our-firm-header'>
+                    <p>Our Firm</p>
+                </div>
+
+                <div className='our-firm-content'>
+                    <div className='our-firm-content-description'>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+                        </p>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+                        </p>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+                        </p>
+                    </div>
+
+                    <div className='our-firm-content-author'>
+                        <img src={stephen_collier} alt="" />
+                        <div className='our-firm-content-author-info'>
+                            <p className='our-firm-content-author-info-name'>Stephen Collier</p>
+                            <p className='our-firm-content-author-info-post'>Senior Partner</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default OurFirm;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import FirmDesc from "../../components/FirmDesc/FirmDesc"
+import OurFirm from "../../components/OurFirm/OurFirm";
 
 function Home() {
     return (
         <>
             <Header />
             <FirmDesc />
+            <OurFirm />
         </>
     );
 }
